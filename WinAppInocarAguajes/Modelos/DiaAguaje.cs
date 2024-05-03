80f77d3dc2493b976bcf6964ae6016c7f780f583
dadajacome <dadajacome@hotmail.com>
2024-05-03 00:46:36 -0500

    .

---
+++ WinAppInocarAguajes/Modelos/DiaAguaje.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WinAppAguajes
+{
+    public class DiaAguaje
+    {
+        public DiaAguaje(DateTime dfeAguaje)
+        {
+            this.dfeAguaje = dfeAguaje;
+        }
+
+        public string guid { get; set; }
+        public DateTime dfeAguaje { get; set; }
+    }
+}
---
+++ WinAppInocarAguajes/Modelos/tblCalendarioAguaje.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WinAppAguajes
+{
+    /*public class tblCalendarioAguaje
+    {
+        public int nqnAguaje { get; set; }
+        public DateTime dfeDesde { get; set; }
+        public DateTime dfeHasta { get; set; }
+    }*/
+}
