d11da15c6c18e6047319c40dfe5ba0df2135937d
dadajacome <dadajacome@hotmail.com>
2024-05-03 00:47:42 -0500

    .

--- WebScrapringSharp/Properties/AssemblyInfo.cs
+++ WebScrapringSharp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WebScrapringSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Ing. Darwin Jácome - Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
